ee7d03d205f3aaabaec6fefcd22b59c73fa3ea62
ElPicolot <elpicolot@gmail.com>
2023-01-18 17:02:06 +0100

    ChangementCOde+Rigidboidy

---
+++ Assets/DeplacementBasic.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementBasic : MonoBehaviour
+{
+    public float speed;
+
+    float MovementX;
+    float MovementY;
+
+    Rigidbody2D Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementX = Input.GetAxisRaw("Horizontal");
+        MovementY = Input.GetAxisRaw("Vertical");
+
+        Rb.velocity = new Vector2(MovementX * speed * Time.deltaTime, MovementY * speed * Time.deltaTime);
+    }
+}
